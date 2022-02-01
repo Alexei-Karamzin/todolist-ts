39d67044b36a4b34f895574099d8873c02ea8513
Alexei-Karamzin <1999aleks_1999@mail.ru>
2022-02-01 23:16:11 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import {TodoList} from "./TodoList";
+
+
+export function App() {
+    const tasks1 = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false}
+    ]
+    const tasks2 = [
+        {id: 1, title: "Hello world", isDone: true},
+        {id: 2, title: "I am Happy", isDone: false},
+        {id: 3, title: "Yo", isDone: false}
+    ]
+
+    return (
+        <div className="App">
+            <TodoList title={'1111'} tasks={tasks1}/>
+            <TodoList title={'2222'} tasks={tasks2}/>
+        </div>
+    );
+}
+
---
+++ src/TodoList.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+type TodolistType = {
+    title: string,
+    tasks: Array<TasksType>
+}
+
+type TasksType = {
+    id: number,
+    title: string,
+    isDone: boolean
+}
+
+export const TodoList = (props: TodolistType) => {
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                {
+                    props.tasks.map((el) => <li><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span>
+                    </li>)
+                }
+                {/*{<li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>}*/}
+                {/*{<li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>}*/}
+                {/*{<li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>}*/}
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    )
+}
